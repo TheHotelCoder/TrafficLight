2a588987a2704b89229e6a1d4fdf40c5c65d3845
TheHotelCoder <dianasofia.ware@gmail.com>
2021-05-15 08:54:03 +0200

    cleaned css styles

--- src/App.js
+++ src/App.js
@@ -1,46 +1,41 @@
-
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
 
 export default class TrafficLight extends React.Component {
-  
-constructor(){
-  super()
-  this.state = {
-    clickedLight: null
-  };
-};
+  constructor() {
+    super();
+    this.state = {
+      clickedLight: null,
+    };
+  }
 
-render() {
-  let redExtraClass = '';
-  if(this.state.clickedLight === "red") redExtraClass = "selected"
-  
-  let yellowExtraClass = '';
-  if(this.state.clickedLight === "yellow") 
-    yellowExtraClass = "selected"
-  
-  let greenExtraClass = '';
-  if(this.state.clickedLight === "green") 
-    greenExtraClass = "selected"
-  
+  render() {
+    let redExtraClass = "";
+    if (this.state.clickedLight === "red") redExtraClass = "selected";
 
+    let yellowExtraClass = "";
+    if (this.state.clickedLight === "yellow") yellowExtraClass = "selected";
 
+    let greenExtraClass = "";
+    if (this.state.clickedLight === "green") greenExtraClass = "selected";
 
-  return (
-    <div className="TrafficLightTop">
-      <div className="container">
-        <div className={"redLight " + redExtraClass} onClick={()=> this.setState({clickedLight: "red"})}>
+    return (
 
+        <div className="container">
+          <div
+            className={"redLight " + redExtraClass}
+            onClick={() => this.setState({ clickedLight: "red" })}
+          ></div>
+          <div
+            className={"yellowLight " + yellowExtraClass}
+            onClick={() => this.setState({ clickedLight: "yellow" })}
+          ></div>
+          <div
+            className={"greenLight " + greenExtraClass}
+            onClick={() => this.setState({ clickedLight: "green" })}
+          ></div>
         </div>
-        <div className={"yellowLight " + yellowExtraClass} onClick={()=> this.setState({clickedLight: "yellow"})}>
-
-        </div>
-        <div className={"greenLight " + greenExtraClass} onClick={()=> this.setState({clickedLight: "green"})}>
-
-        </div>
-      </div>
-   
-    </div>
-  )
-}
+    
+    );
+  }
 }
